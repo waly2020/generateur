577bbbacd3a582139088f3c205b48e1006e116a3
waly2020 <walyguema@gmail.com>
2023-04-11 19:38:11 +0100

    commit pwa online

--- public/pwa/sw.js
+++ public/pwa/sw.js
@@ -1,12 +1,37 @@
 console.log("scripte service worker started");
+const PREFIX = "V1";
+self.addEventListener("install",() =>{
+    console.log(`${PREFIX} installer`);
+})
+self.addEventListener("activate", () =>{
+    console.log(`${PREFIX} activate`);
+})
 self.addEventListener("fetch", (event) =>{
+
     console.log("fetching");
-    console.log(`Feching : ${event.request.url} mode : ${event.request.mode}`);
-    if(event.request.mode === "navigate"){
-        event.respondWith(
-            (async () =>{
-            return new Response("bonjour les gens");
-        })()
-        )
-    }
+
+    // console.log(`Feching : ${event.request.url} mode : ${event.request.mode}`);
+
+    // if(event.request.mode === "navigate"){
+
+    //     event.respondWith(
+
+    //         (async () =>{
+    //             try {
+    //                 const preloadResponse = await event.preloadResponse;
+
+    //                 if(preloadResponse){
+    //                     return preloadResponse;
+    //                 }
+
+    //                 return await fetch(event.request);
+
+    //             } catch (error) {
+                    
+    //                 return new Response("Hello a tous !");
+    //             }
+    //         })
+    //     )
+
+    // }
 })
